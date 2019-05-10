f5c425156cd2cac3cfe07e7ddc399b211291fa91
Guillaume Jospin <gjospin@ucdavis.edu>
2019-05-10 14:51:04 -0700

    Added more packages

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
-pkgs = c("tidyverse", "rmarkdown", "httr", "shinydashboard", "leaflet")
+pkgs = c("tidyverse", "rmarkdown", "httr", "shinydashboard", "leaflet", "gplots", "plotly", "DT")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
